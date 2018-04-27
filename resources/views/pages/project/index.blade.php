dd16ed32ddfdc25e6e3f0475460d9ed575a8c202
alecnicolas <alecnmarcum@gmail.com>
2018-04-27 13:59:57 -0700

    added algolia logo to search page

--- resources/views/pages/project/index.blade.php
+++ resources/views/pages/project/index.blade.php
@@ -19,32 +19,32 @@
     @endif
 @endsection
 @section('filter-tags')
-        <div class="row">
-            @if(request()->filled('department') || request()->filled('sponsor') || request()->filled('type') || request()->filled('collaborators'))
-                <h5 class="text--center"><b>Filters set:</b>
-                    @if(request()->filled('department'))
-                        Department: <a class="tag tag--danger tag--removable"
-                                       href="{{  url("project?".str_replace( "department=".urlencode(Request::input('department')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
-                                       title="Remove Department Filter">{{$filters['setFilters']['department']}}</a>
-                    @endif
-                    @if(request()->filled('sponsor'))
-                        Sponsor: <a class="tag tag--danger tag--removable"
-                                    href="{{  url("project?".str_replace( "sponsor=".urlencode(Request::input('sponsor')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
-                                    title="Remove Sponsor Filter">{{$filters['setFilters']['sponsor']}}</a>
-                    @endif
-                    @if(request()->filled('type'))
-                        Type: <a class="tag tag--danger tag--removable"
-                                    href="{{  url("project?".str_replace( "type=".urlencode(Request::input('type')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
-                                    title="Remove Type Filter">{{$filters['setFilters']['type']}}</a>
-                    @endif
-                    @if(request()->filled('collaborators'))
-                        Collaborators: <a class="tag tag--danger tag--removable"
-                                    href="{{  url("project?".str_replace( "collaborators=".urlencode(Request::input('collaborators')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
-                                    title="Remove Collaborator Filter">{{$filters['setFilters']['collaborators']}}</a>
-                    @endif
-                </h5>
-            @endif
-        </div>
+    <div class="row">
+        @if(request()->filled('department') || request()->filled('sponsor') || request()->filled('type') || request()->filled('collaborators'))
+            <h5 class="text--center"><b>Filters set:</b>
+                @if(request()->filled('department'))
+                    Department: <a class="tag tag--danger tag--removable"
+                                   href="{{  url("project?".str_replace( "department=".urlencode(Request::input('department')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
+                                   title="Remove Department Filter">{{$filters['setFilters']['department']}}</a>
+                @endif
+                @if(request()->filled('sponsor'))
+                    Sponsor: <a class="tag tag--danger tag--removable"
+                                href="{{  url("project?".str_replace( "sponsor=".urlencode(Request::input('sponsor')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
+                                title="Remove Sponsor Filter">{{$filters['setFilters']['sponsor']}}</a>
+                @endif
+                @if(request()->filled('type'))
+                    Type: <a class="tag tag--danger tag--removable"
+                             href="{{  url("project?".str_replace( "type=".urlencode(Request::input('type')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
+                             title="Remove Type Filter">{{$filters['setFilters']['type']}}</a>
+                @endif
+                @if(request()->filled('collaborators'))
+                    Collaborators: <a class="tag tag--danger tag--removable"
+                                      href="{{  url("project?".str_replace( "collaborators=".urlencode(Request::input('collaborators')),'', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY )))  }}"
+                                      title="Remove Collaborator Filter">{{$filters['setFilters']['collaborators']}}</a>
+                @endif
+            </h5>
+        @endif
+    </div>
 @endsection
 @section('session-flashes')
     @if(session('success'))
@@ -71,58 +71,61 @@
 
 @if(count($projects))
     @if($projects instanceof \Illuminate\Pagination\AbstractPaginator)
-        @section('results-titles-and-abstracts')
-            <div class="pagination-total">
-              <small>{{ $projects->total() }} Results Found</small>
-            </div>
-            <br>
-            <div class="row">
-                @each('layouts.partials.project-panel',$projects, 'project' )
-            </div>
-        @endsection
-    @else
-        @section('recent-projects')
-        <h2>Recent Projects</h2>
+@section('results-titles-and-abstracts')
+    <div class="pagination-total">
+        <small>{{ $projects->total() }} Results Found</small>
+    </div>
+    <br>
+    <div class="row">
+        @each('layouts.partials.project-panel',$projects, 'project' )
+    </div>
+@endsection
+@else
+@section('recent-projects')
+    <h2>Recent Projects</h2>
+    <br>
+    <div class="row">
+        @each('layouts.partials.project-panel',$projects, 'project' )
+    </div>
+    <div style="fill:black">
+            <img src="{{ asset('/imgs/algolia_gray.svg') }}" width="100px" style="float: right" alt="algolia">
+    </div>
+@endsection
+@endif
+@section('pagination-links')
+    @if($projects instanceof \Illuminate\Pagination\AbstractPaginator)
+        <br>
         <br>
         <div class="row">
-            @each('layouts.partials.project-panel',$projects, 'project' )
-        </div>
-        @endsection
-    @endif
-    @section('pagination-links')
-        @if($projects instanceof \Illuminate\Pagination\AbstractPaginator)
-            <br>
-            <br>
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="list--unstyled">
-                        {!! $projects->appends(Request::except('page'))->links() !!}
-                    </div>
+            <div class="col-sm-8">
+                <div class="list--unstyled">
+                    {!! $projects->appends(Request::except('page'))->links() !!}
                 </div>
             </div>
-        @endif
-    @endsection
+        </div>
+    @endif
+@endsection
 
 @else
-    @section('no-results-message')
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h1>Your search did not return any results</h1>
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-sm-12">
-                    <p>We recommend you broaden your search.</p>
-                </div>
-                <br><br><br><br>
-                <div class="col-sm-12">
-                    <a href="{{ url("project") }}" class="btn btn-primary">See all Projects</a>
-                </div>
+@section('no-results-message')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Your search did not return any results</h1>
             </div>
         </div>
-    @endsection
+        <br>
+        <div class="row">
+            <div class="col-sm-12">
+                <p>We recommend you broaden your search.</p>
+            </div>
+            <br><br><br><br>
+            <div class="col-sm-12">
+                <a href="{{ url("project") }}" class="btn btn-primary">See all Projects</a>
+            </div>
+        </div>
+    </div>
+@endsection
 @endif
 @section('filter-selection-and-new-project-button')
     @include('layouts.partials.filter-selection-and-new-project-button')
@@ -131,3 +134,4 @@
 @section('page-specific-scripts')
     <script type="text/javascript" src='{{asset('/js/sidebar.js')}}'></script>
 @endsection
+
